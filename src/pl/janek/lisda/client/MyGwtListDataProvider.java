69218328873d4739d355fda9bef915b4b37e0430
jan.kowalskiTP <jan.kowalski@orange.com>
2012-09-12 12:22:46 +0200

    added favicon added html containers

--- src/pl/janek/lisda/client/MyGwtListDataProvider.java
+++ src/pl/janek/lisda/client/MyGwtListDataProvider.java
@@ -1,8 +1,6 @@
 package pl.janek.lisda.client;
 
-import java.util.Arrays;
 import java.util.List;
-
 import pl.janek.lisda.shared.Twit;
 
 import com.google.gwt.cell.client.TextCell;
@@ -11,23 +9,22 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.view.client.ListDataProvider;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class MyGwtListDataProvider implements EntryPoint {
 	private TextArea ta = new TextArea();
+	
 	// The list of data to display.
 	private Button addStockButton = new Button("Add");
-	private static final List<String> DAYS = Arrays.asList("Sunday", "Monday",
-			"Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+	
 	// Create a cell to render each value in the list.
 	private TextCell textCell = new TextCell();
+	
 	// Create a CellList that uses the cell.
 	private CellList<String> cellList = new CellList<String>(textCell);
 
@@ -35,29 +32,22 @@ public class MyGwtListDataProvider implements EntryPoint {
 
 	public void onModuleLoad() {
 
-		// Set the range to display. In this case, our visible range is smaller
-		// than
-		// the data set.
-		// cellList.setVisibleRange(1, 3);
-
 		// Create a data provider.
 		final ListDataProvider<String> dataProvider = new ListDataProvider<String>();
 
 		// Connect the list to the data provider.
 		dataProvider.addDataDisplay(cellList);
 
-		// Add the data to the data provider, which automatically pushes it to
-		// the
-		// widget. Our data provider will have seven values, but it will only
-		// push
+		// Add the data to the data provider, which automatically pushes it to the
+		// widget. Our data provider will have seven values, but it will only push
 		// the four that are in range to the list.
 		List<String> list = dataProvider.getList();
 		intialList(dataProvider.getList());
+		
 		// Add it to the root panel.
-
-		RootPanel.get().add(ta);
-		RootPanel.get().add(addStockButton);
-		RootPanel.get().add(cellList);
+		RootPanel.get("nameFieldContainer").add(ta);
+		RootPanel.get("sendButtonContainer").add(addStockButton);
+		RootPanel.get("twitsContainer").add(cellList);
 
 		// Listen for mouse events on the Add button.
 		addStockButton.addClickHandler(new ClickHandler() {
@@ -78,8 +68,6 @@ public class MyGwtListDataProvider implements EntryPoint {
 
 			@Override
 			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
-
 			}
 		});
 	}
@@ -91,12 +79,11 @@ public class MyGwtListDataProvider implements EntryPoint {
 
 			@Override
 			public void onSuccess(Void result) {
-				System.out.print("send ok");
 			}
 
 			@Override
 			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
+				Window.alert("Data not send to server");
 			}
 		});
 		cellList.setPageSize(list.size());
